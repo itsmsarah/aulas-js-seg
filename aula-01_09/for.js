cf58bc39a3cb3f5852819f3010653a16372d96f5
Sarah Mendes <sarahmendesandrade1@gmail.com>
2025-09-03 14:23:07 -0300

    Add files via upload

---
+++ aula-01_09/for.js
@@ -0,0 +1,30 @@
+// for(let i =0; i<=10;i++){
+//     console.log(i)
+// }
+
+//EXERCICIOS
+
+// for(let i =1; i<=10; i++){
+//     console.log(i)
+// }
+
+// for(let s=0 ;s<=20;s++){
+//     if(s%2==0){
+//         console.log(s)
+//     }
+// }
+
+// for(let i=10; i>0; i--){
+//     console.log(i)
+// }
+
+// for(let i=1;i<=10;i++){
+//    console.log(`5x${i}=${i*5}`)
+// }
+
+// let soma = 0
+
+// for (let i=1 ;i<=100;i++){
+   
+//     console.log(`${soma+=i}`)
+// }
---
+++ aula-01_09/while.js
@@ -0,0 +1,48 @@
+// while(condicao){
+
+// }
+
+// let i = 1
+
+// while(i<=5){
+//     console.log(`Contando${i}`)
+//     i++
+// }
+
+// let senha ="1234"
+// let tentativa =prompt("informe sua senha:")
+
+// while(tentativa!= senha){
+//     alert("Senha incorreta")
+//     tentativa=prompt("Informe sua senha:")
+// }
+// alert("Acesso liberado!")
+
+// let i = 1
+
+// while(i<=10){
+//     console.log(i)
+//     i++
+// }
+
+// let s = 0
+
+// while(s<20){
+//     s+=2
+//     console.log(s)
+// }
+
+// let c = 50
+// while(c>=0){
+//     console.log(c)
+//     c--
+// }
+
+let senha ="beyonce"
+let tentativa = prompt("Informe a senha:")
+
+while(beyonce!=tentativa){
+    alert("Senha incorreta")
+    tentativa =prompt("Informe sua senha")
+}
+alert("Acesso liberado")
